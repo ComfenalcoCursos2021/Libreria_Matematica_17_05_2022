99180dc8c0e93a4c2ab0494b7cc8b654c50627dc
ComfenalcoCursos2021 <educacioncontinua04@unc.edu.co>
2022-05-17 20:41:35 -0500

    Numeros randomicos con decimal

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
-let num1 = 5.6;
-console.log(Math.ceil(num1));
-console.log(Math.round(num1));
+console.log(Math.random());
+console.log(Math.random() * (50 + 1));
+console.log(Math.random() * (100 - 50 + 1)  + 50);
