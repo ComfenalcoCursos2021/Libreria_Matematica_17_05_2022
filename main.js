9d5155552d2057db492655b5597c3fad9a87e6d7
ComfenalcoCursos2021 <educacioncontinua04@unc.edu.co>
2022-05-17 19:55:23 -0500

    Validar negativo y positivo en dicimal

--- main.js
+++ main.js
@@ -1,2 +1,13 @@
-let num = 5.5312468;
-console.log(Math.round(num));
+console.log(Math.floor(-5.4));
+console.log(Math.floor(-5.5));
+console.log(Math.floor(-5.6));
+console.log(Math.round(-5.4));
+console.log(Math.round(-5.5));
+console.log(Math.round(-5.6));
+console.log("----------------");
+console.log(Math.floor(5.4));
+console.log(Math.floor(5.5));
+console.log(Math.floor(5.6));
+console.log(Math.round(5.4));
+console.log(Math.round(5.5));
+console.log(Math.round(5.6));
